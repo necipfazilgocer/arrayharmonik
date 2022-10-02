92a7fb0fd3a082c1be038450b6f3c10339902fb1
necipfazilgocer <114177524+necipfazilgocer@users.noreply.github.com>
2022-10-03 00:22:09 +0300

    Add files via upload

---
+++ arraysort.java
@@ -0,0 +1,17 @@
+
+public class arraysort {
+
+	public static void main(String[] args) {
+	double list[]= {1,2,3};
+	double toplam = 0.0;
+	for(int i=0;i<list.length;i++) {
+	
+		toplam+=1/list[i];
+	
+	 
+	}
+	System.out.println(toplam);
+	
+	}
+
+}
